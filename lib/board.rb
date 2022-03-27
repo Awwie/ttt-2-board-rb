47a1658cb3de95d208e88298d5200d220e8f56a0
Awwie <acecace@live.com>
2022-03-26 22:17:58 -0500

    commit tree 24158f80c4a19ad50df1e289d6c7160cc4b79cee

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,22 @@
 # Define the variable board below.
+
+board = [row[0], row[1], row[2], column[0], column[1], column[2]]
+
+row = [" ", " ", " "]
+
+row[0]
+
+row[1]
+
+row[2]
+
+column = [" ", " ", " "]
+
+column[0]
+
+column[1]
+
+column[2]
+
+row[1] = "X"
+row #=> [" ", "X", " "]
